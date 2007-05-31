fbd7f42449b024818140fa59712db7eef8ed0345
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-05-31 15:29:31 +0000

    First revision.

---
+++ cgworkshop/rev1/fe/Main.cpp
@@ -0,0 +1,24 @@
+#include "FeatureExtraction.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char ** argv) {
+	if (argc < 2)
+	{
+		printf("Usage:\n");
+		printf("	Image : fe <image>\n");
+		return -1;
+	}
+	printf("Doing feature extraction for %s\n", argv[1]);
+	
+	CFeatureExtraction * fe;
+
+	fe = new CFeatureExtraction(argv[1]);
+	fe->Run();
+	
+	return 0;
+	
+}
