18f31b9e26913262b21df55130ad859669b8bb26
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-05-31 19:46:20 +0000

    Gabor note.

--- cgworkshop/rev1/fe/FeatureExtraction.cpp
+++ cgworkshop/rev1/fe/FeatureExtraction.cpp
@@ -149,6 +149,40 @@ int CFeatureExtraction::GetGaborResponse(IplImage *pGrayImg, IplImage *pResImg, 
 // 6 orientations, 4 scales
 // For orientation, I assume equal spacing around PI, but scale?
 
+/*
+orientation_spacing = PI/6
+num_of_frequency_steps = 4
+
+function [filter_parameters] = design_filter_bank(orientation_spacing, num_of_frequency_steps, sampling_rate)
+
+filter_parameters.sx = [];
+filter_parameters.sy = [];
+filter_parameters.frequency = [];
+filter_parameters.orientation = [];
+
+start_frequency = 0.4;
+current_frequency = start_frequency;
+
+num_of_orientations = round(pi / orientation_spacing);
+for k = 1:num_of_frequency_steps
+    filter_bandwidth = 2 / 3 * current_frequency;
+    sx = round(0.5 / pi / (filter_bandwidth^2));
+    sy = round(0.5 * log(2) / (pi^2) / (current_frequency^2) / (tan(orientation_spacing / 2)^2));
+
+    for m = 0:(num_of_orientations - 1)
+        orientation = m * orientation_spacing;
+        filter_parameters.sx = [filter_parameters.sx, sx];
+        filter_parameters.sy = [filter_parameters.sy, sy];
+        filter_parameters.frequency = [filter_parameters.frequency, current_frequency];
+        filter_parameters.orientation = [filter_parameters.orientation, orientation];
+    end
+
+    current_frequency = current_frequency / 2;
+end
+*/
+
+// Anyone care to translate this monster to our 24 filters?
+
 // Save results (one matrix per response)
 // Save to bitmaps
 int CFeatureExtraction::GetGaborResponse()
