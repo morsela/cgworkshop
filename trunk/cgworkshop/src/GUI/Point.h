5076075593ac9b5f70db4914f09f1103dfbd3c75
morsela <morsela@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-06-13 16:24:30 +0000

    Template that!

--- trunk/cgworkshop/src/GUI/Point.h
+++ trunk/cgworkshop/src/GUI/Point.h
@@ -1,45 +1,30 @@
 #ifndef _H_POINT_H_
 #define _H_POINT_H_
 
-
-//TODO: you know what it is, you want it so bad -> TEMPLATES! woohoo!
-struct CPointFloat
+template <typename T>
+class CPoint
 {
-	CPointFloat() {}
+public:
 	
-	CPointFloat (float newX, float newY)
+	CPoint() {}
+	
+	CPoint (T newX, T newY)
 	{
 		SetPoint(newX, newY);
 	}
 	
-	void SetPoint(float newX, float newY)
+	void SetPoint(T newX, T newY)
 	{
 		x = newX;
 		y = newY;
 	}
 
-	float x;
-	float y;
+	T x;
+	T y;
 };
 
-struct CPointInt
-{
-	CPointInt() {}
-	
-	CPointInt (int newX, int newY)
-	{
-		SetPoint(newX, newY);
-	}
-	
-	void SetPoint(int newX, int newY)
-	{
-		x = newX;
-		y = newY;
-	}
-
-	int x;
-	int y;
-};
+typedef CPoint<int>		CPointInt;
+typedef CPoint<float>	CPointFloat;
 
 #endif	//_H_POINT_H_
 
