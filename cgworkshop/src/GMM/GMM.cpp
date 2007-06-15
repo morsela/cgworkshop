dce1e88c55d84ae44d2679f307dab915b5e8f2ca
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-06-15 17:38:40 +0000

    Trying to make sense of GMM.

--- cgworkshop/src/GMM/GMM.cpp
+++ cgworkshop/src/GMM/GMM.cpp
@@ -8,35 +8,58 @@ CGMM::CGMM()
 	m_nEpsilon = 0.05;		
 }
 
-void CGMM::Evaluate(CvMat * pDataSet)
+void CGMM::Init(CvMat * pDataSet)
 {
-	Evaluate(pDataSet, 0);	
+	Init(pDataSet, 0);	
 }
 
-void CGMM::Evaluate(CvMat * pDataSet , CvMat * pActiveMask)
+void CGMM::Init(CvMat * pDataSet , CvMat * pActiveMask)
 {
-	CvEMParams params;
-	
     // initialize model's parameters
-    params.covs      = NULL;
-    params.means     = NULL;
-    params.weights   = NULL;
-    params.probs     = NULL;
-    params.nclusters = m_nClusters;
-    params.cov_mat_type       = CvEM::COV_MAT_DIAGONAL;
-    params.start_step         = CvEM::START_AUTO_STEP;
-    params.term_crit.max_iter = m_nMaxIter;
-    params.term_crit.epsilon  = m_nEpsilon;
-    params.term_crit.type     = CV_TERMCRIT_ITER;
+    m_params.covs      = NULL;
+    m_params.means     = NULL;
+    m_params.weights   = NULL;
+    m_params.probs     = NULL;
+    m_params.nclusters = m_nClusters;
+    m_params.cov_mat_type       = CvEM::COV_MAT_DIAGONAL;
+    m_params.start_step         = CvEM::START_AUTO_STEP;
+    m_params.term_crit.max_iter = m_nMaxIter;
+    m_params.term_crit.epsilon  = m_nEpsilon;
+    m_params.term_crit.type     = CV_TERMCRIT_ITER;
     
-	m_model.train( pDataSet, pActiveMask, params); 	
+    m_model = new CvEM();
+	m_model->train( pDataSet, pActiveMask, m_params); 	
+}
+
+
+void CGMM::NextStep(CvMat * pDataSet)
+{
+	NextStep(pDataSet, 0);	
+}
+
+void CGMM::NextStep(CvMat * pDataSet , CvMat * pActiveMask)
+{
+    // initialize model's parameters
+    m_params.covs      = (const CvMat**)m_model->get_covs();
+    m_params.means     	 = m_model->get_means();
+    m_params.weights   = m_model->get_weights();
+    m_params.probs     = NULL;
+    m_params.start_step         = CvEM::START_E_STEP;
+    
+    
+    // Switch to a new model, train it using the results of the old one
+    CvEM * pNewModel = new CvEM();
+	pNewModel->train( pDataSet, pActiveMask, m_params); 
+	
+	delete m_model;
+	m_model = pNewModel;
 }
 
 float CGMM::GetProbability(CvMat * pFeatureVector)
 {
 	float * _prob = new float[m_nClusters]; // Take this!
 	CvMat prob = cvMat( 1, m_nClusters, CV_32FC1, _prob );
-	m_model.predict( pFeatureVector, &prob );
+	m_model->predict( pFeatureVector, &prob );
 	
 	int i=0;
 	float max = _prob[i];
