2dafba6bbef04049688bb432f31b0327efa1d1e7
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-06-22 19:50:10 +0000

    Beta testing......

--- trunk/cgworkshop/src/GraphHandler.cpp
+++ trunk/cgworkshop/src/GraphHandler.cpp
@@ -14,16 +14,25 @@ GraphHandler::~GraphHandler() {
 
 double calcDist(CvMat * smoothness, int i, int j) {
 
+	//static double sum = 0;
+	//static int count = 0;
 	double result=0;
 
+	double beta = 0.005837*10;
+
 	for (int k = 0; k<3; k++)
+	{
+		//printf("smooth %d: %f,%f\n", k, cvmGet(smoothness, i, k), cvmGet(smoothness, j, k));
 		result += pow(cvmGet(smoothness, i, k) - cvmGet(smoothness, j, k),2);
+	}
+	//sum += result;
+	//count ++;
 	
-	return exp(-result);
+	return exp(-result/beta);
 }
 
 void GraphHandler::init_graph(int rows, int cols, CvMat * smoothness) {
-
+	pixel_count = rows*cols;
 	//map <int, map<int, Graph::node_id>> node;
 	
 	//create m_nodes
@@ -32,7 +41,7 @@ void GraphHandler::init_graph(int rows, int cols, CvMat * smoothness) {
 			m_nodes[i][j] = m_igraph->add_node();
 
 	//need to somehow calculate beta here
-	// double beta = 10.0;
+ 	double beta = 0.005837;
 
 	for(int i = 1; i < rows; i += 2)
 	{
@@ -95,7 +104,7 @@ void GraphHandler::do_MinCut(CvMat &result) {
 			else {
 				cvmSet(&result, i,j,1);
 				counter2++;
-				printf("%d %d \n", i, j);
+				//printf("%d %d \n", i, j);
 			}
 			
 	printf("COUNTER1 %d COUNTER2 %d", counter1, counter2);
