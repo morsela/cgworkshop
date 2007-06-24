896d30a6a01cde6a7916158e2974748770eb904c
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-06-24 20:08:17 +0000

    Debugging GMM

--- trunk/cgworkshop/src/Segmentator.cpp
+++ trunk/cgworkshop/src/Segmentator.cpp
@@ -36,7 +36,11 @@ void Segmentator::Segment()
 	CvMat * pChannels = m_pFe->GetPrincipalChannels();
 	CvMat * f_mask = cvCreateMat( 1, pChannels->rows, CV_8UC1 );
 	CvMat * b_mask = cvCreateMat( 1, pChannels->rows, CV_8UC1 );
-
+	
+	cvSetZero( m_Segmentation );
+	getMask(b_mask,1);
+	
+	
 	cvSetZero( f_mask );
 	//get initial foreground mask
 	for (int i = 0; i < (int)m_points.size();i++)
@@ -46,20 +50,24 @@ void Segmentator::Segment()
 		int y = pI.y;
 		//printf("%d, %d\n", x,y);
 		f_mask->data.ptr[y*m_pImg->width+x]=1;
+		b_mask->data.ptr[y*m_pImg->width+x]=0;
 	}
 			
 
 	//init GMMs
 	f_gmm->Init(pChannels, f_mask);
-	b_gmm->Init(pChannels);
-	f_gmm->NextStep(pChannels, f_mask);
-	b_gmm->NextStep(pChannels);
+	b_gmm->Init(pChannels, b_mask);
+	//f_gmm->NextStep(pChannels, f_mask);
+	//b_gmm->NextStep(pChannels, b_mask);
 
 	//Sink (Background)
 	CvMat * Bu = cvCreateMat(m_pImg->height, m_pImg->width, CV_32F );
 	//Source (Foreground)
 	CvMat * Fu = cvCreateMat( m_pImg->height, m_pImg->width, CV_32F );
+	IplImage * outImg = cvCreateImage(cvSize(m_pImg->width,m_pImg->height), IPL_DEPTH_8U, 1);
 	
+	CvMat * conf_map = cvCreateMat( m_pImg->height, m_pImg->width, CV_32F );
+	char title[50];
 	CvMat * point = cvCreateMat(1,6,CV_32F);
 	for (int n=0; n<MAX_ITER; n++) {
 		double s1 =0, s2=0;
@@ -70,7 +78,7 @@ void Segmentator::Segment()
 			for (int j=0; j<m_pImg->width; j++)                     
 				if (find(m_points.begin(), m_points.end(), CPointInt(j,i))!= m_points.end())
 				{//inside scribble
-					cvmSet(Fu,i,j,100000);
+					cvmSet(Fu,i,j,10000);
 					cvmSet(Bu,i,j,0);
 				
 				}
@@ -83,8 +91,9 @@ void Segmentator::Segment()
 					x++;
 					s1+=-log(b_gmm->GetProbability(point));
 					s2+=-log(f_gmm->GetProbability(point));
-					cvmSet(Bu,i,j,-log(b_gmm->GetProbability(point)));
-					cvmSet(Fu,i,j,-log(f_gmm->GetProbability(point)));
+
+					cvmSet(Fu,i,j,-log(b_gmm->GetProbability(point)));
+					cvmSet(Bu,i,j,-log(f_gmm->GetProbability(point)));
 				}
 				
 			
@@ -95,11 +104,38 @@ void Segmentator::Segment()
 		graph->do_MinCut(*m_Segmentation);
 
 		printf("Flow is %lf\n" ,graph->getFlow());
+		
+		f_gmm->GetAllProbabilities(pChannels, conf_map);
+		
+		cvConvertScale(conf_map, outImg,255,0); 
+		strcpy(title, "fg-conf-map");
+		cvNamedWindow( title, 1 );
+		cvShowImage( title, outImg );
+		cvWaitKey(0);
+		cvDestroyWindow(title);	
+
+		b_gmm->GetAllProbabilities(pChannels, conf_map);
+		
+		cvConvertScale(conf_map, outImg,255,0); 
+		strcpy(title, "bg-conf-map");
+		cvNamedWindow( title, 1 );
+		cvShowImage( title, outImg );
+		cvWaitKey(0);
+		cvDestroyWindow(title);	
+		
+		cvConvertScale(m_Segmentation, outImg,255,0); 
+		strcpy(title, "Segmentation");
+		cvNamedWindow( title, 1 );
+		cvShowImage( title, outImg );
+		cvWaitKey(0);
+		cvDestroyWindow(title);	
+		
 
 		getMask(f_mask,0);
 		getMask(b_mask,1);
 		f_gmm->NextStep(pChannels, f_mask);
 		b_gmm->NextStep(pChannels, b_mask);	
+		
 		delete graph;
 	}
 
