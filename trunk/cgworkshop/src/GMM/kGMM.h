0d2cc8038c3fff49377e51f55e14a14f581a87e8
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-07-28 18:44:42 +0000

    kGMM! Not really working all that well. For NOW.

--- trunk/cgworkshop/src/GMM/kGMM.h
+++ trunk/cgworkshop/src/GMM/kGMM.h
@@ -11,77 +11,52 @@
 class CkGMM 
 {
 public:
-	
-	CkGMM();
+	// Constructors
+	CkGMM(int clusters);
+	CkGMM(int clusters, int max_iterations);
+	CkGMM(int clusters, double accuracy);
+	CkGMM(int clusters, int max_iterations, double accuracy);
 	
 	// Initializes the model for given data set
-	void Init(CvMat * pDataSet);
-
-	// Runs a single EM iteration using the given data set
-	void NextStep(CvMat * pDataSet);
+	void Init(CvMat * pDataSet, CvMat * pActiveMask);
 	
+	// Performs a single GMM iterations on the active samples given
+	void NextStep(CvMat * pActiveMask);
 	
-	// Returns the probability for the given feature vector
-	float GetProbability(CvMat * pFeatureVector);
-	
-	// Returns a matrix that contains, for each feature vector in the data set,
-	// the probability it belongs to either of our two classes
-	// TODO: probably return this in a different format, figure out which
-	void GetAllProbabilities(CvMat * pDataSet, CvMat * pProbs);
+	// Return probabilities for all samples
+	CvMat * GetProbabilities() { return m_pProbabilityMat; }
 	
 private:
+	void ComputeParams(CvMat * pActiveMask);
+	void ComputerProbabilities(CvMat * pActiveMask);
+	void InitClusters(CvMat * pActiveMask);
 
-	int m_nClusters;
+private:
+
+	// General parameters
+	int 	m_nClusters;
+	int		m_nMaxIterations;
+	double		m_nAccuracy;
 	
-	CvMat ** pMeansVecs;
-	CvMat ** pCovMats;
-	CvMat * pWeightVec;
+	// Sample releted vars
+	CvMat * m_pAllSamplesMat;
+	int 	m_nSamplesCount;
+	int 	m_nActiveCount;
+	int		m_nDims;
+
+	// Sample output
+	CvMat *	m_pProbabilityMat;
+	CvMat * m_pLabelMat;
+	
+	// GMM parameters
+	CvMat ** m_pMeanVecs;
+	CvMat ** m_pInvCovMats;
+	double * m_pDetVec;
+	CvMat *	 m_pWeightVec;
+	
+	CvMat ** m_ppCompData;
+
 };
 
-
-/*
- * Code would be along the lines of:
- * 
-
- * 
- * Evaluate(CvMat * pDataSet);
- *
-	int m_nClusters = 5;
-	int m_nMaxIter = 10;
-	float m_nEpsilon = 0.1;
-	
-	CvEMParams params;
-	CvEM em_model;
-
-    // initialize model's parameters
-    params.covs      = NULL;
-    params.means     = NULL;
-    params.weights   = NULL;
-    params.probs     = NULL;
-    params.nclusters = m_nClusters;
-    params.cov_mat_type       = CvEM::COV_MAT_SPHERICAL;
-    params.start_step         = CvEM::START_AUTO_STEP;
-    params.term_crit.max_iter = m_nMaxIter;
-    params.term_crit.epsilon  = m_nEpsilon;
-    params.term_crit.type     = CV_TERMCRIT_ITER|CV_TERMCRIT_EPS;
-    
-	em_model.train( pDataSet, 0, params); 
- *
- * float GetProbability(CvMat * pFeatureVector);
- *
-	float _prob[m_nClusters]; // Take this!
-	CvMat prob = cvMat( 1, m_nClusters, CV_32FC1, _prob );
-	em_model.predict( pFeatureVector, &prob );
-	// return the max value in prob
-	
- *
- * void GetAllProbabilities(CvMat * pDataSet, CvMat * pProbs);
- * 
-
-	// For each feature vector in the set,
-	// call GetProbability
-	// return in a fancy format
-
- */
 
 #endif // __KGMM_H__
