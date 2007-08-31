41a1bc9176b77419edd120e45796d0a7d6221f75
ikirsh <ikirsh@60b542fb-872c-0410-bfbb-43802cb78f6e>
2007-08-31 17:17:24 +0000

    Alpha Expansion commited

--- trunk/cgworkshop/src/Segmentator.h
+++ trunk/cgworkshop/src/Segmentator.h
@@ -7,12 +7,13 @@
 #include "fe/FeatureExtraction.h"
 #include "GUI/TypeDefs.h"
 
-#define MAX_ITER 10
+#define MAX_ITER 5
 #define TOLLERANCE 0.005
 #define BACKGROUND -1
 
 #define ONE_SEG_PER_PIXEL_METHOD 0
 #define AVG_COLOR_METHOD 1
+#define AE_COLOR_METHOD 2
 
 class Segmentator 
 {
@@ -24,32 +25,34 @@ public:
 
 	void Colorize();
 	void Segment();
-	CvMat * getSegmentation(int scribble) {return m_Segmentations[scribble];}
 
 	IplImage * GetSegmentedImage(int scribble);
 	IplImage * GetSegmentedImage();
 
 protected:
 	
-	void SegmentOne(int scribble);
-	void CalcAverage(CvMat * Bg, CvMat * Fg, int scribble);
-	
-	void getMask(CvMat * segmentation, CvMat * mask, int isBackground);
-
-	bool IsInScribble(int i, int j, int scribble);
-	bool IsInScribble(int i, int j);
-	void AssignColors();
-	int decideSegment(int i,int j, int seg1, int seg2);
+	void CreateFinalImage();
+	void SegmentAll();
+	void getMask(CvMat * mask, int label);
+	void PrintStatus(CvMat ** masks);
 	
 	void RecolorPixel(uchar * pData, int y, int x, CvScalar * pColor);
+	void RGB2YUV(CvScalar * pRGB, CvScalar * pYUV);
+
+	void SegmentOne(int scribble);
+	void CalcAverage(CvMat * Bg, CvMat * Fg, int scribble);
+
+
+	void AssignColors();
+	int decideSegment(int i,int j, int seg1, int seg2);
 	
 	void CountSegments();
 	
 	void AssignColor(int i, int j, CvScalar * color, int method);
+	void AssignColorAE(int i, int j, CvScalar * color);
 	void AssignColorOneSeg(int i, int j, CvScalar * color);
 	void AssignColorAvgColor(int i, int j, CvScalar * color);
 
-	void RGB2YUV(CvScalar * pRGB, CvScalar * pYUV);
 
 private:
 
@@ -60,11 +63,13 @@ private:
 	IplImage * m_pImg;
 	IplImage* m_pSegImg;
 	IplImage* m_pTempSegImg;
+
 	CvMat ** m_Segmentations;
 	CvMat * m_SegmentCount;
-	CvMat * m_FinalSeg;
 	CvMat ** m_Probabilities;
 	CvMat ** m_BGProbabilities;
+
+	CvMat * m_pLabels;
 	
 };
 
